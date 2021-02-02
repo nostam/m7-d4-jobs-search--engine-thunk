62b1f2037b489bf54387aea9387ed13147186844
nostam <matsonho@gmail.com>
2021-02-02 21:49:42 +0100

    update style

--- src/components/JobDetail/index.jsx
+++ src/components/JobDetail/index.jsx
@@ -10,18 +10,18 @@ export default function JobDetail(props) {
     <div>
       {props.job !== null && (
         <Paper elevation={8} style={{ marginBottom: "4rem" }}>
-          <div style={{ padding: "2rem" }}>
+          <div style={{ padding: "4rem" }}>
             <Typography variant="h3" gutterBottom>
               {props.job.title}
             </Typography>
             <Typography variant="h5" gutterBottom>
-              {props.job.company}
+              {props.job.type} @ {props.job.company} in {props.job.location}
             </Typography>
             <Typography variant="subtitle1" gutterBottom>
               Posted on: {props.job.created_at}
             </Typography>
             <Typography variant="body1" gutterBottom>
-              {props.job.type} @ {props.job.location}
+              Source: {props.job.url}
             </Typography>
             {parse(props.job.description)}
             <Typography variant="subtitle2" gutterBottom>

41f0922adacd4823fe060571ab51bbce217d2eb6
nostam <matsonho@gmail.com>
2021-02-03 21:19:20 +0100

    plural folder name

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import rootReducer from "../reducer";
+import rootReducer from "../reducers";
 
 export const initialState = {
   bookmarks: [],
@@ -8,18 +8,17 @@ export const initialState = {
   applied: [],
 };
 
-function configureStore() {
-  return createStore(
-    rootReducer,
-    initialState,
-    window.__REDUX__DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-}
 export default createStore(
   rootReducer,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-//TODO cannot export as a function?
-// export default configureStore;
+// TODO cannot export as a function?
+// export default function configureStore() {
+//   return createStore(
+//     rootReducer,
+//     initialState,
+//     window.__REDUX__DEVTOOLS_EXTENSION__ &&
+//       window.__REDUX_DEVTOOLS_EXTENSION__()
+//   );
+// }
